cc29625c220f3d5faf2d4d99f44f316cdf69eb17
Monique Adamson <madamson@hotmail.co.uk>
2018-02-06 22:45:18 +0000

    Create base for code using TDD approach
    Writing unit tests for different scenarios before coding

---
+++ tests/CalculateInterestTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LendInvest;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculateInterestTest extends TestCase
+{
+
+	public function testCanInvestMaximumAmount_returnsSuccess()
+	{
+
+	}
+
+	public function testMaximumInvestmentReached_returnsError()
+	{
+
+	}
+
+	public function testSuccessfulInvestment_returnsSuccess()
+	{
+		
+	}
+
+	public function testMaximumInvestmentAndEndDate_returnsError()
+	{
+
+	}
+
+	public function testInterestCalculation_returnsCorrectData()
+	{
+
+	}
+
+}
